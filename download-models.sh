087022e6a13b7b8824a446687b74d8b002f9c719
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2026-07-26 00:48:04 +0200

    make init in download script

--- download-models.sh
+++ download-models.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -eu
-# Entry point used by the CI build workflow (init-build-script).
-# Delegates to the Makefile which downloads all model components.
-make download-models
+#
+# Deprecated. Remove once workflows switch to v2.
+#
+make init
